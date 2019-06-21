c5980ba50363c080377a2fe1a74f66ebaffe5424
joel perez <jeyledijey@hotmail.com>
2019-06-21 00:29:45 -0700

    DatingAppAPI2 Merged Initial Release

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-value',
+    templateUrl: './value.component.html',
+    styleUrls: ['./value.component.scss']
+})
+/** value component*/
+export class ValueComponent implements OnInit {
+
+  values: any;
+
+    /** value ctor */
+    constructor(private http:HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('https://localhost:44351/api/values').subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+}
---
+++ DatingAppAPI2/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingAppAPI2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAppAPI2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
